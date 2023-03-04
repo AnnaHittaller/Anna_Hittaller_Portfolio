c8399fadeb624be575461695e5e6b8357c15afd0
Anna Hittaller <hittaller.anna@gmail.com>
2023-03-04 15:28:50 +0100

    JS and intersection observer added

--- animations.js
+++ animations.js
@@ -1,4 +1,7 @@
   $(document).ready(function(){
+
+    // bouncing letter animation start *********//
+
     $(".animated").on({
       mouseenter() {
         $(this).addClass("animated");
@@ -7,4 +10,92 @@
         $(this).removeClass("animated");
       },
     });
+
+    // bouncing letter animation end *********//
+
+    // custom mouse code start --------- //
+
+    $(window).mousemove(function(e) {
+    $(".cursor-outer").css({
+      top: e.clientY,
+      left: e.clientX
+    });
+    $(".cursor-inner").css({
+      top: e.clientY +'px',
+      left: e.clientX +'px',
+    
+    });
+  });
+  
+  $("[href],[type=submit], .hamburger").on({
+    mouseenter() {
+      $(".cursor-outer").addClass("cursor-hover");
+    },
+    mouseleave() {
+      $(".cursor-outer").removeClass("cursor-hover");
+    }
+  });
+  
+  $(window).on({
+    mousedown() {
+      $(".cursor-inner").addClass("cursor-click");
+    },
+    mouseup() {
+      $(".cursor-inner").removeClass("cursor-click");
+    }
+  });
+
+  // custom mouse code end --------- //
+
+  // navbar active class toggle start ************* //
+
+  $('.menu-item').click(function() {
+    $('.menu-item').removeClass('active');
+   // $(this).addClass('active');
+    $('.mobile-menu').removeClass('slide-in');
+    $('.hamburger').removeClass('open');
+    $(this).addClass('active');
+  });
+
+
+
+  // navbar active class toggle end *************//
+
+    $('.hamburger').click(function() {
+    $('.mobile-menu').toggleClass('slide-in');
+    //$(this).addClass('active');
+  });
+  
+//navbar hide and slide code start -------------//
+
+var prevScroll = $(window).scrollTop();
+
+$(window).on('scroll', function() {
+  var currentScroll = $(window).scrollTop();
+   if(currentScroll == 0) {
+      $('header').removeClass('box-shadow')
+   } else if(prevScroll > currentScroll) {
+      $('header').removeClass('fadeout');
+      $('header').addClass('box-shadow');
+      //$('.mobile-menu').removeClass('slide-in');
+   } else {
+      if(currentScroll >= 100) {
+        $('header').addClass('fadeout');
+        $('.mobile-menu').removeClass('slide-in');
+        $('.hamburger').removeClass('open');
+      }
+   }
+  prevScroll = currentScroll;
 });
+
+ //navbar hide and slide code end -------------//
+
+
+  $(".hamburger").on('click', function(){
+      $(this).toggleClass("open");
+  });
+
+});
+
+
+
---
+++ observer.js
@@ -0,0 +1,45 @@
+
+
+window.onload = function() {
+
+// simple function to use for callback in the intersection observer
+const navCallback = (entries, observer) => {
+   
+entries.forEach((entry) => {
+//     // verify the element is intersecting
+     if(entry.isIntersecting && entry.intersectionRatio >= 0.3) {
+//       // remove old active class
+      document.querySelector('.active').classList.remove('active');
+     // document.querySelector('.mobile-menu-items a.active').classList.remove('active'); //????
+//       // get id of the intersecting section
+       var id = entry.target.getAttribute('id');
+//       // find matching link & add appropriate class
+       var newLink = document.querySelector(`[href="./index.html#${id}"]`).classList.add('active');
+      // var newLink = document.querySelector(`.mobile-menu-items [href="./index.html#${id}"]`).classList.add('active'); //???
+      
+     }
+   });
+
+   //make a separate one for mobile menu items with rootmargin etc threshold 0
+}
+
+// // init the observer
+ const options = {
+   threshold: 0.3,
+   rootMargin: '0px 0px -30% 0px'
+ }
+
+ const observer = new IntersectionObserver(navCallback, options);
+
+// // target the elements to be observed
+ const sections = document.querySelectorAll('section');
+ sections.forEach((section) => {
+   observer.observe(section);
+ })
+
+
+// for mobile devices -------
+
+
+
+}
